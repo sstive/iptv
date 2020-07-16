284853067b54858e16af6cbe7092d1e5c4257761
SStive39 <sstive39@gmail.com>
2020-07-17 00:51:36 +0300

    Return classes and add data files

---
+++ Classes/form.py
@@ -0,0 +1,7 @@
+class Form:
+
+    def __init__(self, id, name, quality, channels=[]):
+        self.id = id
+        self.name = name
+        self.channels = channels
+        self.quality = quality
